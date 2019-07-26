31efa6c69367c913f2e96fb628d62a03f42a465c
Cardoff-Giant <53348043+Cardoff-Giant@users.noreply.github.com>
2019-07-26 14:11:11 -0400

    Add files via upload

---
+++ Flow.py
@@ -0,0 +1,7 @@
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So you're {30} old, {6'4} tall and {195lbs} heavy. ")
+print(age + height)
+# XXX: 
