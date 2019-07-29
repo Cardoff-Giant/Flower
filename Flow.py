7ce3d52ee375e2b40c241f841ae3a34745a74342
Cardoff-Giant <53348043+Cardoff-Giant@users.noreply.github.com>
2019-07-29 08:55:45 -0400

    Delete Flow.py

--- Flow.py
+++
@@ -1,7 +0,0 @@
-age = input("How old are you?")
-height = input("How tall are you?")
-weight = input("How much do you weigh?")
-
-print(f"So you're {30} old, {6'4} tall and {195lbs} heavy. ")
-print(age + height)
-# XXX: 
